dca0a5fdf5c3dcc01ce90b3a131b66e3122aec86
jeffreyvarner <8898J5ff!>
2014-10-30 15:56:50 -0400

    Starting final version edits

--- Build.sh
+++ Build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Tex this mofo -
-latex $1.tex
+pdflatex $1.tex
 bibtex $1
-latex $1.tex
-latex $1.tex
+pdflatex $1.tex
+pdflatex $1.tex
 
 # Make the pdf -
-dvipdfm -p "letter" $1.dvi
+#dvipdfm -p "letter" $1.dvi
